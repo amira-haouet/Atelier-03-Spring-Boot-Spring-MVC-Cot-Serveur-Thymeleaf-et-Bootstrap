97a6ffe86c5ca74f0e24189e61ce86f18213dd71
Amira Haouet <73131244+htamira@users.noreply.github.com>
2021-04-20 18:03:51 +0100

    Add files via upload

---
+++ src/test/java/com/amira/produits/ProduitsApplicationTests.java
@@ -0,0 +1,75 @@
+package com.amira.produits;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+
+import com.amira.produits.entities.Produit;
+import com.amira.produits.repos.ProduitRepository;
+import com.amira.produits.service.ProduitService;
+
+@SpringBootTest
+class ProduitsApplicationTests {
+	@Autowired
+
+	private ProduitRepository produitRepository;
+
+	@Autowired
+	private ProduitService produitService;
+
+	@Test
+	public void testCreateProduit() {
+		Produit prod = new Produit("PC Amira", 4200.500, new Date());
+		produitRepository.save(prod);
+	}
+
+	@Test
+	public void testFindProduit() {
+		Produit p = produitRepository.findById(1L).get();
+		System.out.println(p);
+	}
+
+	@Test
+	public void testUpdateProduit() {
+		Produit p = produitRepository.findById(1L).get();
+		p.setPrixProduit(1000.0);
+		produitRepository.save(p);
+		System.out.println(p);
+
+	}
+
+	@Test
+	public void testDeleteProduit() {
+		System.out.println(1L);
+
+		produitRepository.deleteById(1L);
+
+		;
+	}
+
+	@Test
+	public void testListerTousProduits() {
+		List<Produit> prods = produitRepository.findAll();
+		for (Produit p : prods) {
+			System.out.println(p);
+		}
+	}
+
+	@Test
+	public void testFindByNomProduitContains() {
+		Page<Produit> prods = produitService.getAllProduitsParPage(0, 2);
+		System.out.println(prods.getSize());
+		System.out.println(prods.getTotalElements());
+		System.out.println(prods.getTotalPages());
+		prods.getContent().forEach(p -> {
+			System.out.println(p.toString());
+		});
+		/*
+		 * ou bien for (Produit p : prods) { System.out.println(p); }
+		 */
+	}
+}
